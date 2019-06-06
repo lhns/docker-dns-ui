e4bdb03b4948997d77bd41ca0a7ed0ea22c6d79a
Pierre Kisters <pierrekisters@gmail.com>
2019-06-06 22:13:31 +0200

    run script with php because of crlf line endings

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ cat config_template.ini | envsubst > config/config.ini
 
 echo "$DNSUI_ADMIN_USER
 $DNSUI_ADMIN_NAME
-$DNSUI_ADMIN_EMAIL" | scripts/create_admin_account.php
+$DNSUI_ADMIN_EMAIL" | php scripts/create_admin_account.php
 
 apache2ctl start
 tail -f /var/log/apache2/error.log | sed -u 's/\\n/\n/g' | sed -u 's/\\t/\t/g'
