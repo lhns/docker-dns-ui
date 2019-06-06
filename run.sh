31f10689db08d7c3f7d22bca7f86a2c68522b459
Pierre Kisters <pierrekisters@gmail.com>
2019-06-07 01:04:10 +0200

    load env vars from $_ENV

--- run.sh
+++ run.sh
@@ -5,16 +5,16 @@ cat config_template.ini | envsubst > config/config.ini
 if ! grep -q 'INSERT INTO "user"' migrations/002.php
 then
   sed -i '/\}$/{e cat '<(echo '
-    $stmt = $this->database->prepare('
+    $stmt = $this->database->prepare('"'"'
     INSERT INTO "user" (uid, name, email, active, admin, auth_realm)
       VALUES (?, ?, ?, ?, ?, ?)
-    ')->execute(array(
-        '"'admin'"',
-        '"'$DNSUI_ADMIN_NAME'"',
-        '"'$DNSUI_ADMIN_EMAIL'"',
+    '"'"')->execute(array(
+        "admin",
+        $_ENV["DNSUI_ADMIN_NAME"],
+        $_ENV["DNSUI_ADMIN_EMAIL"],
         1,
         1,
-        '"'local'"'
+        "local"
     ));
 ')$'\n}' migrations/002.php
 fi
