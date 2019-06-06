83660ac9997dc2e2740a63c1f8db4cd22f27c257
Pierre Kisters <pierrekisters@gmail.com>
2019-06-06 15:22:12 +0200

    fixed sed -u

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 cp -n $DNSUI_HOME/config-sample.ini $DNSUI_HOME/config/config.ini
 
 apache2ctl start
-tail -f /var/log/apache2/error.log | sed -u 's/\\n/\n/g' | -u sed 's/\\t/\t/g'
+tail -f /var/log/apache2/error.log | sed -u 's/\\n/\n/g' | sed -u 's/\\t/\t/g'
